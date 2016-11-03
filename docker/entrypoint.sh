55ebf559359fb8ee31a11375e2f1bd7049004c36
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-11-03 15:39:13 -0200

    Added errorstopping to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Start services and loggers.
 # ---------------------------
 
@@ -13,9 +15,9 @@ sudo service nginx restart > /tmp/nginx.log
 while ! nc -q 1 database-host 3306 </dev/null; do sleep 3; done
 
 echo ""
-echo "------------------------------"
-echo "----- Database connected -----"
-echo "------------------------------"
+echo "--------------------------------------"
+echo "--------- Database connected ---------"
+echo "--------------------------------------"
 echo ""
 
 
@@ -24,15 +26,15 @@ echo ""
 
 if [ ! -f /drupal/app/web/sites/default/settings.local.php ]
 then
-  # 1 - Copy configuration files.
+  # 1 - Install core and other dependencies.
+  composer install
+
+  # 2 - Copy configuration files.
   sudo cp /drupal/app/web/sites/example.settings.local.php.sample /drupal/app/web/sites/default/settings.local.php
   sudo chmod -R 777 /drupal/app/web/sites/default/settings.local.php
 
-  # 2 - Create a directory for files.
+  # 3 - Create a directory for files.
   sudo mkdir -p /drupal/app/web/sites/default/files
-
-  # 3 - Install core and other dependencies.
-  composer install
 
   # 4 - Install standard profile.
   cd /drupal/app/web
@@ -50,9 +52,9 @@ sudo chmod 777 /drupal/app/web/sites/default/settings.local.php
 sudo chmod +w -R /drupal/app/web/sites/default
 
 echo ""
-echo "------------------------------"
-echo "Virtual Marchine ready to work"
-echo "------------------------------"
+echo "--------------------------------------"
+echo "--- Virtual Marchine ready to work ---"
+echo "--------------------------------------"
 echo ""
 
 exec "$@"
